f8d5aa67d803633f02c7037a651d36eee0d6f940
Josh Errickson <josh@errickson.net>
2019-10-24 13:24:19 -0400

    Made tests more robust

--- tests/testthat/test-how_many_imputations.R
+++ tests/testthat/test-how_many_imputations.R
@@ -4,10 +4,21 @@ test_that("multiplication works", {
   data[4:10,3] <- rep(NA,7)
   data[1:5,4] <- NA
   data <- data[-c(5,6)]
-  set.seed(500)
   capture.output(tempData <- mice(data,m=5,maxit=10,meth='pmm')) -> c
   modelFit1 <- with(tempData,lm(Temp~ Ozone+Solar.R+Wind))
-  expect_equal(how_many_imputations(modelFit1), 72)
-  expect_equal(how_many_imputations(pool(modelFit1)), 72)
-  expect_equal(how_many_imputations(modelFit1, cv = .01), 1767)
+
+  hmi1 <- how_many_imputations(modelFit1)
+  expect_type(hmi1, "double")
+  expect_true(hmi1 > 5)
+  expect_true(hmi1 < 500)
+
+  hmi2 <- how_many_imputations(pool(modelFit1))
+  expect_type(hmi2, "double")
+  expect_equal(hmi1, hmi2)
+
+  hmi3 <- how_many_imputations(modelFit1, cv = .01)
+  expect_true(hmi1 < hmi3)
+
+  hmi4 <- how_many_imputations(modelFit1, alpha = .00001)
+  expect_true(hmi1 < hmi4)
 })
