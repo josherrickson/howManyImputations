c70ffe43fca398535093b5f51d8045bccd2428db
Josh Errickson <josh@errickson.net>
2022-02-02 06:26:37 -0500

    Adding a Makefile

---
+++ load.R
@@ -0,0 +1,8 @@
+## Use devtools to load most recent version
+library(utils)
+library("devtools")
+library(testthat)
+devtools:::load_all(export_all=FALSE)
+
+## `utils` is loaded first to ensure proper ordering in the search
+## stack, so that devtool's versions of `help` and `?` mask `utils`.
